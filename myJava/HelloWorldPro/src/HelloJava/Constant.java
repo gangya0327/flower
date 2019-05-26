fdbfec633c90c96e673ab5ed831e3e057818c2c4
raven <king9999427@163.com>
2019-05-27 03:23:05 +0800

    java

---
+++ myJava/HelloWorldPro/src/HelloJava/Constant.java
@@ -0,0 +1,14 @@
+package HelloJava;
+
+public class Constant {
+    public static void main(String[] args) {
+        //常量有整数，小数，字符，字符串，布尔，null
+        System.out.println(10.10);
+        //字符常量，单引号下只能包含一个字符，多个字符（字符串）用双引号
+        System.out.println('a');
+        System.out.println("abc");
+        //long 64位，int 32位，short 16位，
+        // byte 8位，char 2位，
+        // float 32位，double 64位
+    }
+}
