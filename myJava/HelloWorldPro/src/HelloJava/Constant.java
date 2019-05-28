431f7d89e44bbc0d368e9c34064e3e6a84c6d3ae
gangya0327 <king9999427@163.com>
2019-05-28 19:05:59 +0800

    java

--- myJava/HelloWorldPro/src/HelloJava/Constant.java
+++ myJava/HelloWorldPro/src/HelloJava/Constant.java
@@ -10,5 +10,35 @@ public class Constant {
         //long 64位，int 32位，short 16位，
         // byte 8位，char 2位，
         // float 32位，double 64位
+        System.out.println("a" + 1);
+        System.out.println('a' + 1);
+        System.out.println(1 + "a");
+
+//        System.out.println(100/0); //异常
+        System.out.println(100.0 / 0); //Infinity
+
+        System.out.println(10 % -3); //符号（正负）由前一个数决定
+        System.out.println(-10 % -3); //符号（正负）由前一个数决定
+
+        //a++不参与运算，++a参与运算
+        int a = 3;
+        int b = a++;
+        System.out.println("b:" + b);
+        int c = 3;
+        int d = ++c;
+        System.out.println("d:" + d);
+
+        int e = 1;
+        int f = (e++) + 5 + e;
+        System.out.println("f:" + f + ", e:" + e);
+
+        int g = 5;
+        int h = (++g) + (g--) + 5 + g; //6+6+5+5
+
+
+        short i = 4;
+//        i = i + 2; 报错，i是short型，2是int型，
+        i += 2; //i = (short)i + 2
+
     }
 }
--- vue-mstw/blog/src/components/AddBlog.vue
+++ vue-mstw/blog/src/components/AddBlog.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="add-blog">
+  <div class="add-blog" id="add-blog">
     <h2>添加博客</h2>
-    <form action>
+    <form v-if="!submitted">
       <label for>博客标题</label>
       <input type="text" v-model="blog.title" required>
       <label for>博客内容</label>
@@ -16,12 +16,25 @@
         <label for>React.js</label>
         <input type="checkbox" value="React.js" v-model="blog.categories">
       </div>
+      <label>作者：</label>
+      <select v-model="blog.author">
+        <option v-for="(author,index) in authors" :key="index">{{author}}</option>
+      </select>
+      <button v-on:click.prevent="post">添加博客</button>
     </form>
+    <div v-if="submitted">
+      <h3>博客添加成功！！！</h3>
+    </div>
     <div id="preview">
       <h3>博客总览</h3>
       <p>博客标题：{{blog.title}}</p>
       <p>博客内容：</p>
       <p>{{blog.content}}</p>
+      <p>博客分类：</p>
+      <ul>
+        <li v-for="(category,index) in blog.categories" :key="index">{{category}}</li>
+      </ul>
+      <p>作者：{{blog.author}}</p>
     </div>
   </div>
 </template>
@@ -33,14 +46,79 @@ export default {
     return {
       blog: {
         title: "",
-        content: ""
-      }
+        content: "",
+        categories: [],
+        author: ""
+      },
+      authors: ["peter", "rave", "jack", "lucy"],
+      submitted: false
     };
+  },
+  methods: {
+    post() {
+      this.$http
+        .post("http://jsonplaceholder.typicode.com/posts", {
+          title: this.blog.title,
+          body: this.blog.content,
+          userId: 1
+        })
+        .then(response => {
+          this.submitted = true;
+          console.log(response);
+        });
+    }
   }
 };
 </script>
 
-<style>
+<style scoped>
+#add-blog * {
+  box-sizing: border-box;
+}
+#add-blog {
+  margin: 20px auto;
+  max-width: 600px;
+  padding: 20px;
+}
+label {
+  display: block;
+  margin: 20px 0 10px;
+}
+input[type="text"],
+textarea,
+select {
+  display: block;
+  width: 100%;
+  padding: 8px;
+}
+textarea{
+  height: 120px;
+}
+#checkboxes label {
+  display: inline-block;
+  margin-top: 0;
+}
+#checkboxes input {
+  display: inline-block;
+  margin-right: 10px;
+}
+button {
+  display: block;
+  margin: 20px 0;
+  background-color: crimson;
+  color: #fff;
+  border-radius: 4px;
+  padding: 14px;
+  cursor: pointer;
+  border: 0;
+  font-size: 18px;
+}
+#preview {
+  padding: 10px 20px;
+  border: 1px dotted #ccc;
+  margin: 30px 0;
+}
+h3 {
+  margin-top: 10px;
+}
 </style>
-
-
