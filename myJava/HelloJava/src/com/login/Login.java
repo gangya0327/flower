886033f4000a46ffc81b760b5a632ee072264f9b
gangya0327 <king9999427@163.com>
2019-07-02 19:28:14 +0800

    jd

--- myJava/HelloJava/src/com/login/Login.java
+++ myJava/HelloJava/src/com/login/Login.java
@@ -11,6 +11,15 @@ package com.login;
 
 class User {
     String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     private int age;
 
     void show() {
