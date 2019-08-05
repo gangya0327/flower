154e2cebf69404617f776f87f2352e23f5878ff0
gangya0327 <king9999427@163.com>
2019-08-05 18:54:47 +0800

    map

--- react-lucklnk/jingdong/src/pages/home/index/index.js
+++ react-lucklnk/jingdong/src/pages/home/index/index.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import Swiper from '../../../assets/js/libs/swiper.min.js'
-import {request} from '../../../assets/js/libs/request'
+import { request } from '../../../assets/js/libs/request'
 import config from '../../../assets/js/conf/config'
 import '../../../assets/css/common/swiper.min.css'
 import Css from '../../../assets/css/home/index/index.css'
 
 export default class IndexComponent extends React.Component {
-    constructor(){
+    constructor() {
         super()
         this.state = {
-            aSwiper: []
+            aSwiper: [],
+            aNav: [],
+            aGoods: []
         }
     }
     componentDidMount() {
-        this.getSwiper()
+        this.getSwiper();
+        this.getNav();
+        this.getGoodsLevel();
     }
     getSwiper() {
         console.log(config)
-        request(config.baseUrl +  "/api/home/index/slide?token=" + config.token).then((res)=>{
-            if(res.code === 200){
-                this.setState({aSwiper: res.data},() => {
+        request(config.baseUrl + "/api/home/index/slide?token=" + config.token).then((res) => {
+            if (res.code === 200) {
+                this.setState({ aSwiper: res.data }, () => {
                     new Swiper("." + Css['swiper-wrap'], {
                         autoplay: 3000,
                         pagination: ".swiper-pagination",
@@ -27,7 +31,21 @@ export default class IndexComponent extends React.Component {
                     })
                 })
             }
-            console.log(res)
+        })
+    }
+    getNav() {
+        request(config.baseUrl + "/api/home/index/nav?token=" + config.token).then((res) => {
+            if (res.code === 200) {
+                console.log(res.data);
+                this.setState({ aNav: res.data })
+            }
+        })
+    }
+    getGoodsLevel() {
+        request(config.baseUrl + "/api/home/index/goodslevel?token=" + config.token).then((res) => {
+            if (res.code === 200) {
+                this.setState({ aGoods: res.data })
+            }
         })
     }
     render() {
@@ -46,60 +64,105 @@ export default class IndexComponent extends React.Component {
                 <div class={Css['swiper-wrap']}>
                     <div className="swiper-wrapper">
                         {
-                            this.state.aSwiper.map((item, index) => {
-                                return(
-                                    <div key={index} className="swiper-slide">
-                                        <a href={item.webs} target="_blank" rel="noopener noreferrer">
-                                            <img src={item.image} alt={item.title} />
-                                        </a>
-                                    </div>
-                                )
-                            })
+                            this.state.aSwiper !== null ?
+                                this.state.aSwiper.map((item, index) => {
+                                    return (
+                                        <div key={index} className="swiper-slide">
+                                            <a href={item.webs} target="_blank" rel="noopener noreferrer">
+                                                <img src={item.image} alt={item.title} />
+                                            </a>
+                                        </div>
+                                    )
+                                }) : ""
                         }
-                        {/* <div className="swiper-slide">
-                            <a href="http://www.baidu.com" target="_blank" rel="noopener noreferrer">
-                                <img src="http://vueshop.glbuys.com/uploadfiles/1484285334.jpg" alt="" />
-                            </a>
-                        </div>
-                        <div className="swiper-slide">
-                            <a href="http://www.baidu.com" target="_blank" rel="noopener noreferrer">
-                                <img src="http://vueshop.glbuys.com/uploadfiles/1484285334.jpg" alt="" />
-                            </a>
-                        </div>
-                        <div className="swiper-slide">
-                            <a href="http://www.baidu.com" target="_blank" rel="noopener noreferrer">
-                                <img src="http://vueshop.glbuys.com/uploadfiles/1484285334.jpg" alt="" />
-                            </a>
-                        </div> */}
                     </div>
                     <div className="swiper-pagination"></div>
                 </div>
                 <div class={Css['quick-nav']}>
-                    <ul class={Css['item']}>
-                        <li className={Css['item-img']}>
-                            <img src="http://vueshop.glbuys.com/uploadfiles/1484287695.png" alt="" />
-                        </li>
-                        <li className={Css['item-text']}>潮流女装</li>
-                    </ul>
-                    <ul class={Css['item']}>
-                        <li className={Css['item-img']}>
-                            <img src="http://vueshop.glbuys.com/uploadfiles/1484287695.png" alt="" />
-                        </li>
-                        <li className={Css['item-text']}>品牌男装</li>
-                    </ul>
-                    <ul class={Css['item']}>
-                        <li className={Css['item-img']}>
-                            <img src="http://vueshop.glbuys.com/uploadfiles/1484287695.png" alt="" />
-                        </li>
-                        <li className={Css['item-text']}>电脑办公</li>
-                    </ul>
-                    <ul class={Css['item']}>
-                        <li className={Css['item-img']}>
-                            <img src="http://vueshop.glbuys.com/uploadfiles/1484287695.png" alt="" />
-                        </li>
-                        <li className={Css['item-text']}>手机数码</li>
-                    </ul>
+                    {
+                        this.state.aNav !== null ?
+                            this.state.aNav.map((item, index) => {
+                                return (
+                                    <ul class={Css['item']} key={index}>
+                                        <li className={Css['item-img']}>
+                                            <img src={item.image} alt={item.title} />
+                                        </li>
+                                        <li className={Css['item-text']}>{item.title}</li>
+                                    </ul>
+                                )
+                            }) : ""
+                    }
                 </div>
+                {
+                    this.state.aGoods !== null ?
+                        this.state.aGoods.map((item, index) => {
+                            return (
+                                <div className={Css['goods-level-wrap']} key={index}>
+                                    <div className={Css['classify-title'] + " " + Css['color' + (index + 1)]}>—— {item.title} ——</div>
+                                    <div className={Css['goods-level1-wrap']}>
+                                        <div className={Css['goods-level1-item0']}>
+                                            <div className={Css['goods-title']}>高跟鞋女2018新款春季单鞋仙女甜美链子尖头防水台细跟女鞋一字带</div>
+                                            <div className={Css['goods-text']}>精品打折</div>
+                                            <div className={Css['goods-price1']}>128元</div>
+                                            <div className={Css['goods-img']}>
+                                                <img src="http://vueshop.glbuys.com/uploadfiles/1524556409.jpg" alt="" />
+                                            </div>
+                                        </div>
+                                        <div className={Css['goods-level1-item1']}>
+                                            <div className={Css['goods-row']}>
+                                                <div className={Css['goods-row-title']}>欧美尖头蝴蝶结拖鞋女夏外穿2018新款绸缎面细跟凉拖半拖鞋穆勒鞋</div>
+                                                <div className={Css['goods-row-text']}>品质精挑</div>
+                                                <div className={Css['goods-row-img']}>
+                                                    <img src="http://vueshop.glbuys.com/uploadfiles/1524556315.jpg" alt="" />
+                                                </div>
+                                            </div>
+                                            <div className={Css['goods-row']}>
+                                                <div className={Css['goods-row-title']}>欧美尖头蝴蝶结拖鞋女夏外穿2018新款绸缎面细跟凉拖半拖鞋穆勒鞋</div>
+                                                <div className={Css['goods-row-text']}>品质精挑4折起</div>
+                                                <div className={Css['goods-row-img']}>
+                                                    <img src="http://vueshop.glbuys.com/uploadfiles/1524556315.jpg" alt="" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className={Css['goods-list-wrap']}>
+                                        <div className={Css['goods-list']}>
+                                            <div className={Css['title']}>小白鞋女2018春夏季新款韩版百搭平底学生原宿ulzzang帆布鞋板鞋</div>
+                                            <div className={Css['image']}>
+                                                <img src="http://vueshop.glbuys.com/uploadfiles/1524556119.jpg" alt="" />
+                                            </div>
+                                            <div className={Css['price']}>￥280</div>
+                                            <div className={Css['unprice']}>￥280</div>
+                                        </div>
+                                        <div className={Css['goods-list']}>
+                                            <div className={Css['title']}>小白鞋女2018春夏季新款韩版百搭平底学生原宿ulzzang帆布鞋板鞋</div>
+                                            <div className={Css['image']}>
+                                                <img src="http://vueshop.glbuys.com/uploadfiles/1524556119.jpg" alt="" />
+                                            </div>
+                                            <div className={Css['price']}>￥280</div>
+                                            <div className={Css['unprice']}>￥480</div>
+                                        </div>
+                                        <div className={Css['goods-list']}>
+                                            <div className={Css['title']}>小白鞋女2018春夏季新款韩版百搭平底学生原宿ulzzang帆布鞋板鞋</div>
+                                            <div className={Css['image']}>
+                                                <img src="http://vueshop.glbuys.com/uploadfiles/1524556119.jpg" alt="" />
+                                            </div>
+                                            <div className={Css['price']}>￥280</div>
+                                            <div className={Css['unprice']}>￥298</div>
+                                        </div>
+                                        <div className={Css['goods-list']}>
+                                            <div className={Css['title']}>小白鞋女2018春夏季新款韩版百搭平底学生原宿ulzzang帆布鞋板鞋</div>
+                                            <div className={Css['image']}>
+                                                <img src="http://vueshop.glbuys.com/uploadfiles/1524556119.jpg" alt="" />
+                                            </div>
+                                            <div className={Css['price']}>￥280</div>
+                                            <div className={Css['unprice']}>￥310</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        }) : ""
+                }
                 <div className={Css['goods-level-wrap']}>
                     <div className={Css['classify-title'] + " " + Css['color1']}>—— 潮流女装 ——</div>
                     <div className={Css['goods-level1-wrap']}>
