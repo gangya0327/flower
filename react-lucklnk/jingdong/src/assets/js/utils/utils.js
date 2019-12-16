9711751cdb29543d4c3df59295aa8e0fd70eb492
raven <king9999427@163.com>
2019-12-17 00:19:38 +0800

    单点登录

--- react-lucklnk/jingdong/src/assets/js/utils/utils.js
+++ react-lucklnk/jingdong/src/assets/js/utils/utils.js
@@ -1,10 +1,15 @@
 import echo from '../libs/echo'
+import {request} from '../libs/request'
+import action from '../../../actions/index'
+import config from '../conf/config'
+
 function lazyImage() {
     echo.init({
-        offset: 100,//可视区域多少像素可被加载
+        offset: 100, //可视区域多少像素可被加载
         throttle: 800 //设置图片延迟加载时间
     })
 }
+
 function localParam(search, hash) {
     search = search || window.location.search
     hash = search || window.location.hash
@@ -25,11 +30,30 @@ function localParam(search, hash) {
 
 console.log(global.scrollTop.index);
 
-function setScrollTop(val=0) {
-    setTimeout(()=>{
+function setScrollTop(val = 0) {
+    setTimeout(() => {
         document.body.scrollTop = val
         document.documentElement.scrollTop = val
     }, val)
 }
 
-export { lazyImage, localParam, setScrollTop }
+//会员登录安全认证
+function safeAuth(props) {
+    let sUrl = config.baseUrl + "/api/home/user/safe?token=" + config.token
+    request(sUrl, "post", {
+            uid: props.state.user.uid,
+            auth_token: props.state.user.authToken
+        })
+        .then(res => {
+            if (res.code !== 200) {
+                props.dispatch(action.user.outLogin())
+                props.history.replace(config.path + "login/index")
+            }
+        })
+}
+export {
+    lazyImage,
+    localParam,
+    setScrollTop,
+    safeAuth
+}
--- react-lucklnk/jingdong/src/pages/user/index/index.js
+++ react-lucklnk/jingdong/src/pages/user/index/index.js
@@ -2,10 +2,20 @@ import React from 'react'
 import { connect } from 'react-redux'
 import action from '../../../actions/index'
 import config from '../../../assets/js/conf/config'
+import { request } from '../../../assets/js/libs/request'
+import {safeAuth} from '../../../assets/js/utils/utils'
 // import Css from '../../../assets/css/home/index/index.css'
 
 class UserComponent extends React.Component {
+    constructor(props) {
+        super(props)
+        safeAuth(props)
+    }
     outLogin() {
+        let sUrl = config.baseUrl + "/api/home/user/safeout?token=" + config.token
+        request(sUrl, "post", { uid: this.props.state.user.uid }).then(res => {
+            console.log(res)
+        })
         this.props.dispatch(action.user.outLogin())
         this.props.history.replace(config.path + "login/index")
     }
