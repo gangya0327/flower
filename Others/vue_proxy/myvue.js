87233a8f9c050af195d414b306d92ba8bba1c84d
gangya0327 <king9999427@163.com>
2020-01-02 11:25:20 +0800

    fix bug

--- Others/vue_proxy/myvue.js
+++ Others/vue_proxy/myvue.js
@@ -84,94 +84,94 @@ class Vue extends EventTarget {
 
 // tmc保存代码
 
-// 点开警铃，有通知的一项
-if (statementNum > 0) {
-    $("#mainNav>li:eq(0)>a").click();
+// // 点开警铃，有通知的一项
+// if (statementNum > 0) {
+//     $("#mainNav>li:eq(0)>a").click();
 
-    if (statementCreateNum > 0) {
-        //默认显示第一个tab
-    } else if (statementOverdueNum > 0) {
-        $("#corp-statement .myTab>li:eq(1)>a").click();
-    }
-} else if (Abnormal_Num > 0) {
-    $("#mainNav>li:eq(1)>a").click();
+//     if (statementCreateNum > 0) {
+//         //默认显示第一个tab
+//     } else if (statementOverdueNum > 0) {
+//         $("#corp-statement .myTab>li:eq(1)>a").click();
+//     }
+// } else if (Abnormal_Num > 0) {
+//     $("#mainNav>li:eq(1)>a").click();
 
-    if (checkOrderNum > 0) {
-        //默认显示第一个tab
-    } else if (checkFligNum > 0) {
-        $("#domestic-flight .myTab>li:eq(1)>a").click();
-    } else if (checkPnrNum > 0) {
-        $("#domestic-flight .myTab>li:eq(2)>a").click();
-    } else if (customerOrderNum > 0) {
-        $("#domestic-flight .myTab>li:eq(3)>a").click();
-    }
-} else if (customer_Attention > 0) {
-    $("#mainNav>li:eq(2)>a").click();
-}
+//     if (checkOrderNum > 0) {
+//         //默认显示第一个tab
+//     } else if (checkFligNum > 0) {
+//         $("#domestic-flight .myTab>li:eq(1)>a").click();
+//     } else if (checkPnrNum > 0) {
+//         $("#domestic-flight .myTab>li:eq(2)>a").click();
+//     } else if (customerOrderNum > 0) {
+//         $("#domestic-flight .myTab>li:eq(3)>a").click();
+//     }
+// } else if (customer_Attention > 0) {
+//     $("#mainNav>li:eq(2)>a").click();
+// }
 
-if ($(this).attr("href") != "javascript:") {
-    $.session.set('selectedMenu', $(this).attr("href"));
-}
+// if ($(this).attr("href") != "javascript:") {
+//     $.session.set('selectedMenu', $(this).attr("href"));
+// }
 
-formatter: function (value, row, index) {
-    var str = "";
-    if (value == 0) {
-        str = "生成订单待提交";
-    } else if (value == 1) {
-        str = "待确认";
-    } else if (value == 2) {
-        str = "已审";
-    } else if (value == 3) {
-        str = "酒店已确认";
-    } else if (value == 4) {
-        str = "酒店未核实入住信息";
-    } else if (value == 5) {
-        str = "已入住";
-    } else if (value == 6) {
-        str = "未入住";
-    } else if (value == 7) {
-        str = "已结账";
-    } else if (value == 8) {
-        str = "变更";
-    } else if (value == 9) {
-        str = "变价";
-    } else if (value == 10) {
-        str = "特殊满房";
-    } else if (value == 11) {
-        str = "满房";
-    } else if (value == 12) {
-        str = "取消";
-    } else if (value == 13) {
-        str = "删除";
-    } else if (value == 14) {
-        str = "等待代理人手动处理"
-    } else if (value == 15) {
-        str = "导入订单"
-    } else if (value == 16) {
-        str = "取消中"
-    } else if (value == 17){
-        str = "取消失败"
-    }
-    if(row.isExternal == 1) {
-        str += "（外采回录）"
-    }
-    return str;
-}
+// formatter: function (value, row, index) {
+//     var str = "";
+//     if (value == 0) {
+//         str = "生成订单待提交";
+//     } else if (value == 1) {
+//         str = "待确认";
+//     } else if (value == 2) {
+//         str = "已审";
+//     } else if (value == 3) {
+//         str = "酒店已确认";
+//     } else if (value == 4) {
+//         str = "酒店未核实入住信息";
+//     } else if (value == 5) {
+//         str = "已入住";
+//     } else if (value == 6) {
+//         str = "未入住";
+//     } else if (value == 7) {
+//         str = "已结账";
+//     } else if (value == 8) {
+//         str = "变更";
+//     } else if (value == 9) {
+//         str = "变价";
+//     } else if (value == 10) {
+//         str = "特殊满房";
+//     } else if (value == 11) {
+//         str = "满房";
+//     } else if (value == 12) {
+//         str = "取消";
+//     } else if (value == 13) {
+//         str = "删除";
+//     } else if (value == 14) {
+//         str = "等待代理人手动处理"
+//     } else if (value == 15) {
+//         str = "导入订单"
+//     } else if (value == 16) {
+//         str = "取消中"
+//     } else if (value == 17){
+//         str = "取消失败"
+//     }
+//     if(row.isExternal == 1) {
+//         str += "（外采回录）"
+//     }
+//     return str;
+// }
 
-//未下发
-formatter: function (value, row) {
-    var str = "<a href='/statement/statementDetail?statementType=unissued&corpName=" + encodeURI(encodeURI(row.corpName)) + "&corpId=" + row.corpId +
-        "&statementGenerateUnit=" + row.statementGenerateUnit + "&statementStartDate=" + row.statementStartDate +
-        "&statementEndDate=" + row.statementEndDate + "' target='_blank'>查看操作账单</a>";
-    return str;
-}
+// //未下发
+// formatter: function (value, row) {
+//     var str = "<a href='/statement/statementDetail?statementType=unissued&corpName=" + encodeURI(encodeURI(row.corpName)) + "&corpId=" + row.corpId +
+//         "&statementGenerateUnit=" + row.statementGenerateUnit + "&statementStartDate=" + row.statementStartDate +
+//         "&statementEndDate=" + row.statementEndDate + "' target='_blank'>查看操作账单</a>";
+//     return str;
+// }
 
-// 未收款
-formatter: function (value, row) {
-    var statementStatus = row.issuedDate ? "已下发未收款" : "未下发未收款";
-    var str = "<a href='/statement/statementDetail?statementType=unsettle&corpName=" + encodeURI(encodeURI(row.corpName)) + "&corpId=" + row.corpId +
-        "&statementGenerateUnit=" + row.statementGenerateUnit + "&statementStatus=" + encodeURI(encodeURI(statementStatus)) +
-        "&statementStartDate=" + row.statementStartDate + "&statementEndDate=" + row.statementEndDate +
-        "&repaymentDate=" + row.repaymentDate + "&isAudit=" + row.settlementAuditStatus + "' target='_blank'>查看操作账单</a>";
-    return str;
-}
+// // 未收款
+// formatter: function (value, row) {
+//     var statementStatus = row.issuedDate ? "已下发未收款" : "未下发未收款";
+//     var str = "<a href='/statement/statementDetail?statementType=unsettle&corpName=" + encodeURI(encodeURI(row.corpName)) + "&corpId=" + row.corpId +
+//         "&statementGenerateUnit=" + row.statementGenerateUnit + "&statementStatus=" + encodeURI(encodeURI(statementStatus)) +
+//         "&statementStartDate=" + row.statementStartDate + "&statementEndDate=" + row.statementEndDate +
+//         "&repaymentDate=" + row.repaymentDate + "&isAudit=" + row.settlementAuditStatus + "' target='_blank'>查看操作账单</a>";
+//     return str;
+// }
